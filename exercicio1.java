963e3ecc4b4d6970d64d3f10679785559d38a4ee
TeTeago <55003392+TeTeago@users.noreply.github.com>
2026-03-24 20:23:59 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class exercicio1 {
+
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            
+            System.out.println("Digite um número inteiro: ");
+           
+            if (scanner.hasNextInt()) {
+                int numero = scanner.nextInt();
+                if (numero % 2 == 0) {
+                    System.out.println (numero + " é par.");
+                } else {
+                    System.out.println(numero + " é ímpar.");
+                }
+                
+                scanner.close();
+            
+            }
+
+        }
+
+    }
+
+}
