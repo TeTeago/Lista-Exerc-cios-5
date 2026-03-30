43284d910619d2154b0a182111404d244d089df2
TeTeago <55003392+TeTeago@users.noreply.github.com>
2026-03-30 17:26:42 -0300

    Add files via upload

---
+++ exercicio3.java
@@ -0,0 +1,48 @@
+import java.util.Scanner; 
+
+public class exercicio3 {
+    
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro número: ");
+            double num1 = scanner.nextDouble();
+            
+            System.out.println("Digite o segundo número: ");
+            double num2 = scanner.nextDouble();
+            
+            System.out.print("Operações: ");
+            System.out.println("1 - Soma");
+            System.out.println("2 - Subtração");
+            System.out.println("3 - Multiplicação");
+            System.out.println("4 - Divisão");
+            
+            System.out.println("Escolha: ");
+            int escolha = scanner.nextInt();
+            
+            switch (escolha) {
+                case 1 -> System.out.println("Resultado: " + (num1 + num2));
+                case 2 -> System.out.println("Resultado: " + (num1 - num2));
+                case 3 -> System.out.println("Resultado: " + (num1 * num2));
+                case 4 -> {
+                    if (num2 != 0) {
+                        System.out.println("Resultado: " + (num1 / num2));
+                    } else {
+                        System.out.println("Erro: divisão por zero não é permitida.");
+                    }
+                }
+                default -> System.out.println("Opção inválida.");
+            }
+        }
+
+        
+
+    }
+
+
+
+
+}
+
+
+
