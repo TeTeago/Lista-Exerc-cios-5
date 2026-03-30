c4ba7d58a7ffb1e749886641d56f821fd9916d49
TeTeago <55003392+TeTeago@users.noreply.github.com>
2026-03-30 17:42:26 -0300

    Add files via upload

---
+++ exercicio4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class exercicio1 {
+
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.print("Digite N: ");
+            int N = scanner.nextInt();
+
+                if (N < 0) {
+                    System.out.println("Valor inválido. N deve ser maior que zero. ");
+                }
+    
+                int contador = 1;
+
+                while (contador <= N) {
+                    System.out.println(contador);
+                    contador++;
+                }
+
+                scanner.close();
+            
+            }
+    
+        }
+    }
